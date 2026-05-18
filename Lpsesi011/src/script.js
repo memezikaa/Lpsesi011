42920daa64650a9d6616e3fc80675e8d14c9fdd3
memezikaa <pedrohenriqueandrettau@gmail.com>
2026-05-18 08:25:47 -0300

    Add files via upload

---
+++ Lpsesi011/src/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', ()=> {
+    const nav = document.querySelector('nav')
+
+    if(window.scrollY > 0){
+        nav.classList.add('sc')
+    }else{
+        nav.classList.remove('sc')
+    }
+})
